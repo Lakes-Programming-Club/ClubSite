4431c0309ae093e70fe03fc7c8612ab50a787f44
Alex Curran <haletas88@gmail.com>
2025-05-27 21:40:18 +1200

    Added the events page

---
+++ src/data/events.js
@@ -0,0 +1,21 @@
+// Events more like events!
+
+export const events = [
+    {
+        date: "2025-05-31",
+        title: "Monthly Bar",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Ut sed nulla mi. Mauris enim lacus, vestibulum a orci nec, rhoncus auctor nunc.
+                    Donec id ex vitae diam molestie viverra.
+                    Proin eget libero sit amet elit sodales accumsan eget pretium libero.`
+    },
+    {
+        date: "2025-04-20",
+        title: "Intro to Foo Development",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Ut sed nulla mi. Mauris enim lacus, vestibulum a orci nec, rhoncus auctor nunc.
+                    Donec id ex vitae diam molestie viverra.
+                    Proin eget libero sit amet elit sodales accumsan eget pretium libero.`
+    }
+
+];
