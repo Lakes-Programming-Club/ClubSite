0713d1e10a09be20182f2069c7d0e609285ee5b1
Alex Curran <haletas88@gmail.com>
2025-05-31 22:22:39 +1200

    Added resources

---
+++ src/data/resources.ts
@@ -0,0 +1,29 @@
+// Resources more like resources!
+
+export const resources = [
+    {
+        title: "LeetCode",
+        url: `https://leetcode.com/`,
+        description: "leetcode description",
+        image: `../public/resource-icons/leetcode.png`
+    },
+    {
+        title: "CS50",
+        url: `https://cs50.harvard.edu/x/2025/`,
+        description: "Harvard University's introduction to the intellectual enterprises of computer science and the art of programming.",
+        image: `../public/resource-icons/cs50duck.png`
+    },
+    {
+        title: "Dave Leeds",
+        url: `https://www.youtube.com/@typealias`,
+        description: "dave leeds description",
+        image: `../public/resource-icons/DaveLeeds.jpg`
+    },
+    {
+        title: "Stack Overflow",
+        url: `https://stackoverflow.com`,
+        description: "Stack Overflow description",
+        image: `../public/resource-icons/stackoverflow.png`
+    },
+];
+
