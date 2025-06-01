d7f710bc25099f6af608f8cf2945ccf91f05b31b
Alex Curran <haletas88@gmail.com>
2025-06-01 13:46:23 +1200

    filters

--- src/data/resources.ts
+++ src/data/resources.ts
@@ -5,25 +5,33 @@ export const resources = [
         title: "LeetCode",
         url: `https://leetcode.com/`,
         description: "leetcode description",
-        image: `../public/resource-icons/leetcode.png`
+        image: `/resource-icons/leetcode.png`,
+        level: "all",
+        language: "all"
     },
     {
         title: "CS50",
         url: `https://cs50.harvard.edu/x/2025/`,
         description: "Harvard University's introduction to the intellectual enterprises of computer science and the art of programming.",
-        image: `../public/resource-icons/cs50duck.png`
+        image: `/resource-icons/cs50duck.png`,
+        level: "beginner",
+        language: "all"
     },
     {
         title: "Dave Leeds",
         url: `https://www.youtube.com/@typealias`,
-        description: "dave leeds description",
-        image: `../public/resource-icons/DaveLeeds.jpg`
+        description: "Dave Leeds description",
+        image: `/resource-icons/DaveLeeds.jpg`,
+        level: "intermediate",
+        language: "kotlin"
     },
     {
         title: "Stack Overflow",
         url: `https://stackoverflow.com`,
         description: "Stack Overflow description",
-        image: `../public/resource-icons/stackoverflow.png`
+        image: `/resource-icons/stackoverflow.png`,
+        level: "all",
+        language: "all"
     },
 ];
 
